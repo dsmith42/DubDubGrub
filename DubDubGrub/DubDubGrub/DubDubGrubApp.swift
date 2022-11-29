fa3060d67c934e866dd8fcf99f99c4c72526c4f2
Dan Smith <dsmith42@tutanota.com>
2022-11-29 08:07:54 +0000

    Create project

---
+++ DubDubGrub/DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Dan Smith on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
