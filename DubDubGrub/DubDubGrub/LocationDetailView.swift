68d23e8ce422c53c5ffc356ae1709168bbfe5d3b
Dan Smith <dsmith42@tutanota.com>
2022-11-29 11:48:30 +0000

    Refactor LocationDetailView a little

--- DubDubGrub/DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/DubDubGrub/LocationDetailView.swift
@@ -17,25 +17,15 @@ struct LocationDetailView: View {
     var body: some View {
 			VStack(spacing: 16) {
 
-				Image("default-banner-asset")
-					.resizable()
-					.scaledToFill()
-					.frame(height: 120)
+				BannerImageView(imageName: "default-banner-asset")
 
 				HStack {
-					Label("123 Any Street", systemImage: "mappin.and.ellipse")
-						.font(.caption)
-						.foregroundColor(.secondary)
-
+					AddressView(address: "123 Any Street")
 					Spacer()
 				}
 				.padding(.horizontal)
 
-				Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. ")
-					.lineLimit(3)
-					.minimumScaleFactor(0.75)
-					.frame(height: 70)
-					.padding(.horizontal)
+				DescriptionView(text: "This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description.")
 
 				ZStack {
 					Capsule()
@@ -129,5 +119,36 @@ struct FirstNameAvatarView: View {
 				.lineLimit(1)
 				.minimumScaleFactor(0.75)
 		}
+	}
+}
+
+struct BannerImageView: View {
+	var imageName: String
+
+	var body: some View {
+		Image(imageName)
+			.resizable()
+			.scaledToFill()
+			.frame(height: 120)
+	}
+}
+
+struct AddressView: View {
+	var address: String
+	var body: some View {
+		Label(address, systemImage: "mappin.and.ellipse")
+			.font(.caption)
+			.foregroundColor(.secondary)
+	}
+}
+
+struct DescriptionView: View {
+	var text: String
+	var body: some View {
+		Text(text)
+			.lineLimit(3)
+			.minimumScaleFactor(0.75)
+			.frame(height: 70)
+			.padding(.horizontal)
 	}
 }
