7665b4bbecdf049315d043bcb487996fad4a3cb2
Dan Smith <dsmith42@tutanota.com>
2022-11-29 15:42:59 +0000

    Indentation

--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 import MapKit
 
 struct LocationMapView: View {
-	@State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
-																				  longitude: -121.891054),
-												   span: MKCoordinateSpan(latitudeDelta: 0.01,
-																		  longitudeDelta: 0.01))
+	@State private var region = MKCoordinateRegion(
+		center: CLLocationCoordinate2D(
+			latitude: 37.331516,
+			longitude: -121.891054),
+		span: MKCoordinateSpan(
+			latitudeDelta: 0.01,
+			longitudeDelta: 0.01))
+
     var body: some View {
 		ZStack {
 			Map(coordinateRegion: $region).ignoresSafeArea()
